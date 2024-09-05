7125db5349995f455899db0cd1c79e48da538000
CerTabMike <161540719+CerTabMike@users.noreply.github.com>
2024-09-05 08:27:06 -0500

    Update Tarea01.py

--- SESION04/Tarea01.py
+++ SESION04/Tarea01.py
@@ -5,3 +5,19 @@
  * Te recomiendo que busques información para conocer en profundidad cómo
  * realizar el cifrado. Esto también forma parte del reto.
  */
+
+def cifrado_cesar(texto, desplazamiento, modo='cifrar'):
+  
+  resultado = ""
+  for caracter in texto:
+    if caracter.isalpha():
+      
+      caracter = caracter.lower()
+      
+      nuevo_indice = ord(caracter) - ord('a') + desplazamiento
+      
+      nuevo_indice %= 26
+      
+      nuevo_caracter = chr(nuevo_indice + ord('a'))
+      
+    
